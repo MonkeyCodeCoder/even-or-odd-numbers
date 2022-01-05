23bd63f554b68637f86f94a92e9ea37c535bf231
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-01-05 18:54:36 +0000

    drm/radeon: properly filter DP1.2 4k modes on non-DP1.2 hw

--- src/Controller/AccesContract.php
+++ src/Controller/AccesContract.php
@@ -40,4 +40,4 @@ class AccesContract
         $foo3= $model->setPaymentDate("f1046a81aefa46a1e257855f2bb955ec07180ad96c47bc85b8faf2656b918459","tests write paymet date");
         $foo4= $model->setFactoringExpirationDate("f1046a81aefa46a1e257855f2bb955ec07180ad96c47bc85b8faf2656b918459","tests write factoring expiration date");
         $foo5= $model->setFactoringTotal("f1046a81aefa46a1e257855f2bb955ec07180ad96c47bc85b8faf2656b918459","tests write factoring total");
- 
+        ret
