7dc79a9e30c1ee6ccee22711b8b9a3e60a61b9d9
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-03-02 08:06:54 +0000

    Improved unit tests.

--- src/Controller/AccesContract.php
+++ src/Controller/AccesContract.php
@@ -54,4 +54,4 @@ class AccesContract
             "payment term test"," invoice date test","payment date test",
             "expiration date test","factoring expiration date test");*/
         //$foo = $model->getDocumentList();
-        //$f
+        //$foo = $model->exists
