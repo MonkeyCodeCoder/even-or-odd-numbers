f8971ae4fefdc75183af742caa90582ca1bb74ea
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-03-20 13:22:35 +0000

    Readme file

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,4 +5,4 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Htt
+use Symfony\Component\HttpKernel\Kern
