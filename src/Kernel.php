ef3488199245900741d609f9ea4b3b516c8696cc
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-03-23 05:28:02 +0000

    merge in gingerbread-release history after reset to gingerbread

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,4 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class Kernel 
