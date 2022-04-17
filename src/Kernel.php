f47ed95d79d8eeb9ecad9a26990e96f440ef5a1a
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-04-17 12:58:37 +0000

    net: socket: Fix the wrong returns for recvmsg and sendmsg

--- src/Kernel.php
+++ src/Kernel.php
@@ -29,4 +29,5 @@ class Kernel extends BaseKernel
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
-                yield new $class
+                yield new $class();
+            }
