e5c619354c9463baa785c0f413e3adfd13959aa6
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-05-06 13:46:25 +0000

    Added some logging

--- src/Kernel.php
+++ src/Kernel.php
@@ -38,4 +38,5 @@ class Kernel extends BaseKernel
     {
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir
+        $confDir = $this->getProjectDir().'/config';
+   
