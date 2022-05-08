7e2702566a41c5772d885a059322aeaf9afbd7c1
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-05-08 10:17:05 +0000

    Update to 1.12

--- src/Kernel.php
+++ src/Kernel.php
@@ -39,4 +39,5 @@ class Kernel extends BaseKernel
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
         $confDir = $this->getProjectDir().'/config';
-        $loader->load($confDir.'/packages/*'.self::CONFIG_EXTS, 'glob')
+        $loader->load($confDir.'/packages/*'.self::CONFIG_EXTS, 'glob');
+        if (is_d
