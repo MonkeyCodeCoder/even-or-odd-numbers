eb8412e551ba3c1f5b10170b53142e066e1d7329
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-05-21 22:50:13 +0000

    added more features

--- src/Kernel.php
+++ src/Kernel.php
@@ -44,3 +44,4 @@ class Kernel extends BaseKernel
             $loader->load($confDir.'/packages/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
         }
         $loader->load($confDir.'/services'.self::CONFIG_EXTS, 'glob');
+        $lo
