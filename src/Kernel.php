2ab324fef4a5ce63bea6ea85544d6054a615274a
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-05-29 12:11:20 +0000

    Removed log statement

--- src/Kernel.php
+++ src/Kernel.php
@@ -49,4 +49,4 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
-        $confDir = $this->getP
+        $confDir = $this->getProjectDir().'/
