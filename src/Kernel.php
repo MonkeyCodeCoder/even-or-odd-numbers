68597d3682bffcfd69325ca4533a93f9a1c91d70
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-06-12 11:29:57 +0000

    drm/mgag200: Reject non-character-cell-aligned mode widths

--- src/Kernel.php
+++ src/Kernel.php
@@ -55,4 +55,5 @@ class Kernel extends BaseKernel
         }
         if (is_dir($confDir.'/routes/'.$this->environment)) {
             $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-      
+        }
+        
