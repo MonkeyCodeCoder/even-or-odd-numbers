4bb54d57c0d00146235c9af209b820cc4e510355
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-06-15 12:00:55 +0000

    add options

--- src/Kernel.php
+++ src/Kernel.php
@@ -56,4 +56,4 @@ class Kernel extends BaseKernel
         if (is_dir($confDir.'/routes/'.$this->environment)) {
             $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
         }
-        
+        $routes->import($conf
