d9e3408cab5c00e0aa15bb541b4ce40c1992ebee
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-06-18 11:22:06 +0000

    Coveralls badge

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -6,3 +6,25 @@
  * Date: 04/01/2018
  * Time: 14:03
  */
+
+namespace App\Model;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ApiModel
+{
+    const URl="http://localhost:8545";
+    const ACCOUNT="0xDd421A95ab8D53919092Cf2A144815905C2BC4Db";
+    const CONTRACT="0x690Ea531A7ba08BEA5789BB0f708E73CCe864276";
+
+    function getDocumentCount(){
+        // keccak-256 de getDocumentCount() es 3d1c227335f9755b3b49b8845a25fff553fbe76676aff139dcdcb6ac8783f91c, se toman los 8 primeros caracteres
+        $data  = [
+            'jsonrpc'=>'2.0','method'=>'eth_call','params'=>[[
+                "from"=> self::ACCOUNT, "to"=> self::CONTRACT,"data"=> "0x3d1c2273"],'latest'
+            ],'id'=>67
+        ];
+        $params= json_encode($data);
+        $handler = curl_init();
+        curl_setopt($handler, CURLOPT_URL, self::URl);
