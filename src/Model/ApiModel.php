6c20bb630591691cb288acdbf745dee5534f7b59
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-09-16 05:56:07 +0000

    sync: don't block the flusher thread waiting on IO

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -704,3 +704,22 @@ class ApiModel
         $response = curl_exec ($handler);
         curl_close($handler);
         $json=json_decode($response,true);
+        $result=$json['result'];
+        // ejemplo de resultado
+        //0x
+        //0000000000000000000000000000000000000000000000000000000000000020  indica donde empieza la definicion de la respuesta(a los 32 B)
+        //0000000000000000000000000000000000000000000000000000000000000009  indica el tamaño de la respuesta(9 B -> 18 caracteres)
+        //3031372d30303535330000000000000000000000000000000000000000000000  la respuesta en hex
+        $lenghtAndArgResult = substr($result,66);// string con la longitud de la respuesta y la respuesta
+        $lenghtResult= substr($lenghtAndArgResult,0,64);// logitud del resultado
+        $argResult=substr($lenghtAndArgResult,64,hexdec($lenghtResult)*2); // argumento
+
+        return $this->Hex2String($argResult);
+
+    }
+
+    function getFactoringExpirationDate($id){
+        // hex del id
+        $idHex = $this->String2Hex($id);
+        //tomar el numero de caracteres, dividir por 2 para obtener el numero de bytes y pasar ese numero a hex y dezplazarlo
+        $lengthIdHex=str_pad(dechex(strlen($idHex )/2), 64, "0", STR_PAD_LEFT);
