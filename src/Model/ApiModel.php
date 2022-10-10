12e4347c04cabbe1cbd455b90d13649c09c7f643
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-10-10 08:26:32 +0000

    Update transit_priority_areas.md

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -820,3 +820,25 @@ class ApiModel
         $data  = [
             'jsonrpc'=>'2.0','method'=>'eth_sendTransaction','params'=>[[
                 "from"=> self::ACCOUNT, "to"=> self::CONTRACT,"gas"=>"0x927c0","data"=> $call]],'id'=>67
+        ];
+        $params= json_encode($data);
+        $this->unlockAccount();
+        $handler = curl_init();
+        curl_setopt($handler, CURLOPT_URL, self::URl);
+        curl_setopt($handler, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($handler, CURLOPT_POST,true);
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec ($handler);
+        curl_close($handler);
+        $json=json_decode($response,true);
+        $result=$json['result'];
+        return "hash de la transferencia : ".$result;
+    }
+
+//-----------------------------------------------------insert--------------------------------------------------
+    function insertDocument($id, $invoiceNumber,$fiscalYear,$total ,$factoringTotal ,$state ,$currency,$paymentType,
+                            $supplierName,$customerName,$financialInstitutionName,$factoringState,$paymentTerms,
+                            $invoiceDate,$paymentDate,$expirationDate,$factoringExpirationDate){
+        // hex de los parametros
+        $idHex = $this->String2Hex($id);
