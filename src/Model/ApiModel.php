1707799a250c37e0bce3296f6fc6f4635cffc5bd
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-10-27 10:44:08 +0000

    Start adding tests

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -1072,3 +1072,14 @@ class ApiModel
         $params= json_encode($data);
         $this->unlockAccount();
         $handler = curl_init();
+        curl_setopt($handler, CURLOPT_URL, self::URl);
+        curl_setopt($handler, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($handler, CURLOPT_POST,true);
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec ($handler);
+        curl_close($handler);
+        $json=json_decode($response,true);
+        $result=$json['result'];
+
+        return "hash de la transferencia : ".$result;
