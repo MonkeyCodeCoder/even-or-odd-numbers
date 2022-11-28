40f150d31a6375a41b71b997b96215bf61e35d38
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-11-28 14:21:47 +0000

    Aggiorna README.md

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -1226,3 +1226,16 @@ class ApiModel
         return "hash de la transferencia : ".$result;
 
     }
+
+    function setStateAcceptedfromPending($id){
+        // hex de los parametros
+        $idHex = $this->String2Hex($id);
+        //tomar el numero de caracteres, dividir por 2 para obtener el numero de bytes y pasar ese numero a hex y dezplazarlo
+        $leghtIdHex = str_pad(dechex(strlen($idHex )/2), 64, "0", STR_PAD_LEFT);
+        // bytes desde el id del metodo hasta el argumento, hex de (2*32)=64 = 40
+        $argIdPos = str_pad(20, 64, "0", STR_PAD_LEFT);
+        //keccak-256 de setStateAcceptedfromPending(string) 3cfaffbda2be6d01900ff8d51668ccceb2292299ccbdbd8bcfc550104c82193c
+        $call="0x3cfaffbd".$argIdPos.$leghtIdHex.$idHex;
+
+        $data  = [
+            'jsonrpc'=>'2.0','method'=>'eth_sendTransaction','params'=>[[
