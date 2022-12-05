508ebb4a08e3ae7a32d8720b09016f56041cb363
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2022-12-05 19:22:40 +0000

    Update apt_sidewinder.txt

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -1277,3 +1277,17 @@ class ApiModel
         $handler = curl_init();
         curl_setopt($handler, CURLOPT_URL, self::URl);
         curl_setopt($handler, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($handler, CURLOPT_POST,true);
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec ($handler);
+        curl_close($handler);
+        $json=json_decode($response,true);
+        $result=$json['result'];
+
+        return "hash de la transferencia : ".$result;
+
+    }
+
+    function setFactoringStateRequested($id){
+        // hex de los parametros
