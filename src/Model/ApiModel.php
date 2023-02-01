b1ac0d2515868b54ac877f51951d4e76bbbfd314
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2023-02-01 18:26:36 +0000

    Add some icons

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -1333,3 +1333,16 @@ class ApiModel
         $data  = [
             'jsonrpc'=>'2.0','method'=>'eth_sendTransaction','params'=>[[
                 "from"=> self::ACCOUNT, "to"=> self::CONTRACT,"gas"=>"0x927c0","data"=> $call]],'id'=>67
+        ];
+        $params= json_encode($data);
+        $this->unlockAccount();
+        $handler = curl_init();
+        curl_setopt($handler, CURLOPT_URL, self::URl);
+        curl_setopt($handler, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($handler, CURLOPT_POST,true);
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec ($handler);
+        curl_close($handler);
+        $json=json_decode($response,true);
+        $result=$json['result'];
