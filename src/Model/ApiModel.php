b7d91b6efd1e41c650418457f16fffdf87eaad50
MonkeyCodeCoder <90749302+MonkeyCodeCoder@users.noreply.github.com>
2023-02-17 19:39:59 +0000

    fix indentation issues

--- src/Model/ApiModel.php
+++ src/Model/ApiModel.php
@@ -1380,3 +1380,9 @@ class ApiModel
 
     function Hex2String($hex){
         $string='';
+        for ($i=0; $i < strlen($hex)-1; $i+=2){
+            $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+        }
+        return $string;
+    }
+}
